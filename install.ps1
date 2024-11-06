a33aa70ab750a8a4d5eead18977b4639e3a88809
IImoxi <167373150+IImoxi@users.noreply.github.com>
2024-11-06 23:14:23 +0000

    installer test 2

--- install.ps1
+++ install.ps1
@@ -1,3 +1,9 @@
-Write-Host "Running";
-Start-Sleep -Seconds 1
+Write-Host "[JPizxeL RT] Installer/Updater";
+iwr -Uri https://github.com/IImoxi/JpizxelRtTextures/archive/refs/heads/main.zip -OutFile ./main.zip
+$appdata = [Environment]::GetFolderPath('ApplicationData')
+$resourcePacks = Join-Path $appdata "Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\resource_packs";
+Expand-Archive -LiteralPath './main.zip' -DestinationPath $resourcePacks
+Remove-Item ./main.zip
+Write-Host "[JPizxeL RT] Resource pack is now installed.";
+Start-Sleep -Seconds 5
 exit
