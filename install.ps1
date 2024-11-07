0e6813abe02900edf2d72817b542e6f830cc2153
IImoxi <167373150+IImoxi@users.noreply.github.com>
2024-11-07 14:39:58 +0000

    Packs is updatable with the installer now

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 Write-Host "[JPizxeL RT] Installer/Updater";
 iwr -Uri https://github.com/IImoxi/JpizxelRtTextures/archive/refs/heads/main.zip -OutFile ./main.zip
 $resourcePacks = Join-Path "$($env:LOCALAPPDATA)" "Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\resource_packs";
-Expand-Archive -LiteralPath './main.zip' -DestinationPath $resourcePacks
+Expand-Archive -LiteralPath './main.zip' -DestinationPath $resourcePacks -Force
 Remove-Item ./main.zip
 Write-Host "[JPizxeL RT] Resource pack is now installed.";
 Start-Sleep -Seconds 5
